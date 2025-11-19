a6841e8f7865ae1fd9e71cd7e868ff4766e139f8
Palash Chandra Roy <164641475+shahedroks@users.noreply.github.com>
2025-11-19 17:53:21 +0600

    project readdy for develoment

---
+++ lib/core/constants/image_control/image_path.dart
@@ -0,0 +1,6 @@
+
+class ImagePath {
+ static final String authBackground = "assets/images/auth_background.svg";
+ static final String svgLogo = "assets/images/svgLogo.svg";
+ static final String pngLogo = "assets/images/logo.png";
+}
