ee9a147dee20a906232a193ec6d25bd5f48a4d71
Palash Chandra Roy <164641475+shahedroks@users.noreply.github.com>
2025-12-02 01:29:45 +0600

    fff

--- lib/features/nav_bar/logic/botton_nav_index_logic.dart
+++ lib/features/nav_bar/logic/botton_nav_index_logic.dart
@@ -1,3 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final bottomNavIndexProvider = StateProvider<int>((ref) => 0);
+final internalBottomNavIndexProvider = StateProvider<int>((ref) => 0);
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -5,11 +5,13 @@ import 'package:workpleis/features/auth/screens/otp_verify_screen.dart';
 import 'package:workpleis/features/auth/screens/role_selection_screen.dart';
 import 'package:workpleis/features/auth/screens/set_password_screen.dart';
 import 'package:workpleis/features/customer/screen/Customer_guest_home_screen.dart';
+import 'package:workpleis/features/internal_technician/screen/internal_jobs.dart';
 import 'package:workpleis/features/nav_bar/screen/bottom_nav_bar.dart';
-
 import '../features/auth/screens/customer_create_account_screen.dart';
 import '../features/auth/screens/phone_login_screen.dart';
 import '../features/customer/screen/guest_profile_screen.dart';
+import '../features/internal_technician/screen/internal_technician_home.dart';
+import '../features/nav_bar/screen/internal_bottom_nav_bar.dart';
 import 'error_screen.dart';
 
 class AppRouter {
@@ -78,6 +80,26 @@ class AppRouter {
         name: GuestProfileScreen.routeName,
         builder: (context, state) => const GuestProfileScreen(),
       ),
+      //Internal Technician flow;
+      GoRoute(
+        path: InternalDashboardV2Screen.routeName,
+        name: InternalDashboardV2Screen.routeName,
+        builder: (context, state) => const InternalDashboardV2Screen(),
+      ),
+
+      GoRoute(
+        path: InternalBottomNavBar.routeName,
+        name: InternalBottomNavBar.routeName,
+        builder: (context, state)=> const InternalBottomNavBar()
+      ),
+
+      GoRoute(
+          path: InternalJobs.routeName,
+          name: InternalJobs.routeName,
+          builder: (context, state)=> const InternalJobs ()
+      ),
+
+
     ],
   );
 }
